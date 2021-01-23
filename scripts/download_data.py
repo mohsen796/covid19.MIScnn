542dd67e1ff638b7d191dd9046b04ecbd93da9a0
Mohsen <36323468+mohsen796@users.noreply.github.com>
2021-01-23 18:16:43 +0330

    Update download_data.py

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -44,26 +44,8 @@ def download_from_url(url, dst):
     @param: url to download file
     @param: dst place to put the file
     """
-    file_size = int(requests.head(url).headers["Content-Length"])
-    if os.path.exists(dst):
-        first_byte = os.path.getsize(dst)
-    else:
-        first_byte = 0
-    if first_byte >= file_size:
-        print("WARNING: Skipping download due to files are already there.")
-        return file_size
-    header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
-    pbar = tqdm(
-        total=file_size, initial=first_byte,
-        unit='B', unit_scale=True, desc=url.split('/')[-1])
-    req = requests.get(url, headers=header, stream=True)
-    with(open(dst, 'ab')) as f:
-        for chunk in req.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                pbar.update(1024)
-    pbar.close()
-    return file_size
+    import urllib
+    urllib.urlretrieve(url, filename=dst)
 
 #-----------------------------------------------------#
 #                     Runner Code                     #
